d7240f554c3915b368ae645e29f9b4d3170d78cc
Jelle Verheyen <jelle.verheyen@protonmail.ch>
2020-12-19 16:40:44 +0100

    Rename DayTwo to Day2 and create Day3 project

---
+++ 2020/C#/Day3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
